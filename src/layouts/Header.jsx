b112e75db5d99b0aa1558cf946df59f9430932ab
student079 <wjdwlstjd968@hanyang.ac.kr>
2025-03-05 09:43:51 +0900

    헤헷 버튼

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,15 +1,43 @@
-import { Link } from "react-router-dom";
-import "./Header.css";
+import { Navbar, Nav, Button, Container } from "react-bootstrap";
 
 function Header() {
   return (
-    <header className="header">
-      <nav className="nav">
-          <Link to="/team/create">팀 생성</Link>
-          <Link to="/team/applications">신청 목록 조회</Link>
-          <Link to="/team/guesting">게스팅 신청</Link>
-      </nav>
-    </header>
+    <Navbar expand="lg" className="shadow" style={{ backgroundColor: "#EF66A5" }}>
+      <Container>
+        <Nav className="ms-auto d-flex gap-3">
+          <Button
+            style={{
+              backgroundColor: "#ef66a5",
+              borderColor: "#ef66a5",
+              fontSize: "14px",
+              padding: "5px 10px",
+            }}
+          >
+            팀 생성
+          </Button>
+          <Button
+            style={{
+              backgroundColor: "#ef66a5",
+              borderColor: "#ef66a5",
+              fontSize: "14px",
+              padding: "5px 10px",
+            }}
+          >
+            신청 목록 조회
+          </Button>
+          <Button
+            style={{
+              backgroundColor: "#ef66a5",
+              borderColor: "#ef66a5",
+              fontSize: "14px",
+              padding: "5px 10px",
+            }}
+          >
+            게스팅 신청
+          </Button>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 }
 
